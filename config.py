5def8df6852916bb1ef2151d6adec42e2265775c
Redeye828 <manisharya1302@gmail.com>
2025-03-14 06:40:13 -0700

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ load_dotenv()
 
 API_ID = 25667269
 API_HASH = "9941534ef2a1b5466e157baf26f29db2"
-BOT_TOKEN = "7017405982:AAF2XZv-lGxm5nBjGfXxPQFQ6XzP74gueLo"
-MONGO_URL = "mongodb+srv://rohit6205881743:rohit6205881743@cluster0.soqtewz.mongodb.net/"
+BOT_TOKEN = ""
+MONGO_URL = "mongodb+srv://aryatushar409:Wxu2b5IhUxl5cyUr@cluster0.apkfr.mongodb.net/"
 MUST_JOIN = "PrimaxPortal"
 SUDO_USERS = [7047049923]
 UPI_ID = "9568536542@fam"
